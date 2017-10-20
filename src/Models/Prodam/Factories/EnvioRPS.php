f84d113a2cdac1ddd60563a768510162052ca6f7
rapharappoli <31960910+rapharappoli@users.noreply.github.com>
2017-10-20 16:05:40 -0200

    alteração EnvioRPS

--- src/Models/Prodam/Factories/EnvioRPS.php
+++ src/Models/Prodam/Factories/EnvioRPS.php
@@ -47,26 +47,14 @@ class EnvioRPS extends Factory
         $data = null
     ) {
         $xmlRPS = '';
-        $method = "PedidoEnvioRPS";
+        $method = "PedidoEnvioLoteRPS";
         $content = $this->requestFirstPart($method);
-        if (is_object($data)) {
-            $xmlRPS .= $this->individual($data);
-        } elseif (is_array($data)) {
-            if (count($data) == 1) {
-                $xmlRPS .= $this->individual($data[0]);
-            } else {
-                $method = "PedidoEnvioLoteRPS";
-                $content = $this->requestFirstPart($method);
-                $xmlRPS .= $this->lote($data);
-            }
-        } else {
-            return '';
-        }
+        $xmlRPS .= $this->lote($data);
         $content .= Header::render(
             $versao,
             $remetenteTipoDoc,
             $remetenteCNPJCPF,
-            null,
+            $transacao,
             null,
             null,
             null,
